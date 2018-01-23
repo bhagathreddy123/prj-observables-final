4f6ed706239f67dc9fdcca33bf8c4037eae23605
bhagath <bhagath@getcustomexposure.com>
2018-01-23 14:10:46 +0530

    resetting the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -42,7 +42,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if(this.editMode) {
@@ -50,6 +50,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     }else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
     
   }
 
